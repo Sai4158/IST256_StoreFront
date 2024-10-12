5d991763d39367bf7ea7807fbcea1d92f7481ec0
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-10-12 03:02:50 -0400

    Also updated the JS file for JSON

--- script.js
+++ script.js
@@ -5,10 +5,16 @@ document
 
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const age = document.getElementById("age").value;
+    const address = document.getElementById("address").value;
 
     const shopper = {
       name: name,
       email: email,
+      phone: phone,
+      age: age,
+      address: address,
     };
 
     document.getElementById("shopperDetails").textContent = JSON.stringify(
