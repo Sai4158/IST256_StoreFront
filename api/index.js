df40b132ffb7f73e5358fae7f046bb321a6217c9
Sai4158 <116776995+Sai4158@users.noreply.github.com>
2024-11-02 14:10:21 -0400

    Trying to fix

--- api/index.js
+++ api/index.js
@@ -13,8 +13,8 @@ app.use(
     origin: [
       "http://127.0.0.1:5500",
       "http://localhost:5500",
-      "https://storefront256.vercel.app/",
-    ], // Add your Vercel app URL here
+      "https://storefront256.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
